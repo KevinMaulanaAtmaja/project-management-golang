4cc6a4b1400b31f778093f79d2457c91e99ae1f5
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2026-01-18 12:00:00 +0700

    feat: add user repository and user service file

---
+++ services/user_service.go
@@ -0,0 +1,45 @@
+package services
+
+import (
+	"errors"
+
+	"github.com/KevinMaulanaAtmaja/project-management-golang/models"
+	"github.com/KevinMaulanaAtmaja/project-management-golang/repositories"
+	"github.com/KevinMaulanaAtmaja/project-management-golang/utils"
+	"github.com/google/uuid"
+)
+
+type UserService interface {
+	Register(user *models.User) error
+}
+
+type userService struct {
+	repo repositories.UserRepository
+}
+
+func NewUserService(repo repositories.UserRepository) UserService {
+	return &userService{repo}
+}
+
+func (s *userService) Register(user *models.User) error {
+	// harus cek email yg terdaftar apkah sudah dipake/blum
+	// hashing password
+	// set role
+	// simpan user
+
+	existingUser, _ := s.repo.FindByEmail(user.Email)
+	if existingUser.InternalID != 0 {
+		return errors.New("Email already registered")
+	}
+
+	hashed, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+
+	user.Password = hashed
+	user.Role = "user"
+	user.PublicID = uuid.New()
+
+	return s.repo.Create(user)
+}
