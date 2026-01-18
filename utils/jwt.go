54d67f7f6f58f2f0b22dab386b8b0cf30fb70faf
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2026-01-18 22:30:00 +0700

    feat: add token and refresh token generators

---
+++ utils/jwt.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/KevinMaulanaAtmaja/project-management-golang/config"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+// generate token jwt
+// generate refresh token jwt
+func GenerateToken(userID int64, role, email string, publicID uuid.UUID) (string, error) {
+	secret := config.AppConfig.JWTSecret
+	duration, _ := time.ParseDuration(config.AppConfig.JWTExpire)
+
+	claims := jwt.MapClaims{
+		"user_id":   userID,
+		"role":      role,
+		"email":     email,
+		"public_id": publicID,
+		"exp":       time.Now().Add(duration).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
