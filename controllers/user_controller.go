1487766796099570705c85c89ec24796f30279cb
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2026-01-18 23:40:00 +0700

    feat: add register method to user controller

---
+++ controllers/user_controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"github.com/KevinMaulanaAtmaja/project-management-golang/models"
+	"github.com/KevinMaulanaAtmaja/project-management-golang/services"
+	"github.com/KevinMaulanaAtmaja/project-management-golang/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserController struct {
+	service services.UserService
+}
+
+func NewUser(s services.UserService) *UserController {
+	return &UserController{service: s}
+}
+
+func (c *UserController) Register(ctx *fiber.Ctx) error {
+	user := new(models.User)
+
+	if err := ctx.BodyParser(user); err != nil {
+		return utils.BadRequest(ctx, "Gagal Parsing Data", err.Error())
+	}
+
+	if err := c.service.Register(user); err != nil {
+		return utils.BadRequest(ctx, "Registrasi Gagal", err.Error())
+	}
+
+	return utils.Success(ctx, "Register Success", user)
+}
