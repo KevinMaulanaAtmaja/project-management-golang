83368b0d15ea706436b6b466c9e36abe7dadbb80
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2026-01-19 12:00:00 +0700

    feat: add get user endpoint
    - add get user route and controller
    - implement find user by id and public id in repo
    - integrate gofiber jwt middleware

--- routes/route.go
+++ routes/route.go
@@ -3,8 +3,11 @@ package routes
 import (
 	"log"
 
+	"github.com/KevinMaulanaAtmaja/project-management-golang/config"
 	"github.com/KevinMaulanaAtmaja/project-management-golang/controllers"
+	"github.com/KevinMaulanaAtmaja/project-management-golang/utils"
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
 	"github.com/joho/godotenv"
 )
 
@@ -14,6 +17,19 @@ func Setup(app *fiber.App, uc *controllers.UserController) {
 		log.Fatal("Error loading .env file.")
 	}
 
-	app.Post("/v1/auth/register", uc.Register)
-	app.Post("/v1/auth/login", uc.Login)
+	app.Post("/api/v1/auth/register", uc.Register)
+	app.Post("/api/v1/auth/login", uc.Login)
+
+	// JWT protected routes
+	api := app.Group("/api/v1", jwtware.New(jwtware.Config{
+		SigningKey: []byte(config.AppConfig.JWTSecret),
+		ContextKey: "user",
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return utils.Unauthorized(c, "Error Unauthorized", err.Error())
+		},
+	}))
+
+	useGroup := api.Group("/users")
+	useGroup.Get("/:id", uc.GetUser) //  /api/v1/users/:id
+
 }
